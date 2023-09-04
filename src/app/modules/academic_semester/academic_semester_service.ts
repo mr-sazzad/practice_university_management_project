0437427cc0e185e745c632ca612ef72f5f49c732
mr-sazzad <main.sazza@gmail.com>
2023-09-04 21:14:12 +0600

    get single AcademicSemester by id service

--- src/app/modules/academic_semester/academic_semester_service.ts
+++ src/app/modules/academic_semester/academic_semester_service.ts
@@ -1,4 +1,5 @@
 import { AcademicSemester, Prisma } from '@prisma/client';
+import ApiError from '../../../errors/ApiError';
 import { paginationHelpers } from '../../../helpers/paginationHelper';
 import { IGenericResponse } from '../../../interfaces/common';
 import { IFilters, IPaginationOptions } from '../../../interfaces/pagination';
@@ -75,7 +76,30 @@ const getAllSemesters = async (
   };
 };
 
+const getSingleAcademicSemester = async (
+  id: string
+): Promise<AcademicSemester | null> => {
+  const isExist = await prisma.academicSemester.findFirst({
+    where: {
+      id,
+    },
+  });
+
+  if (!isExist) {
+    throw new ApiError(404, 'Record Not Found !');
+  }
+
+  const result = await prisma.academicSemester.findUnique({
+    where: {
+      id,
+    },
+  });
+
+  return result;
+};
+
 export const academicSemester = {
   createAcademicSemester,
   getAllSemesters,
+  getSingleAcademicSemester,
 };
