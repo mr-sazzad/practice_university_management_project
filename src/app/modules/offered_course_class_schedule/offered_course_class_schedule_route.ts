a542f5136c639abe5ef5f8210d523a1eda15b305
mr-sazzad <main.sazza@gmail.com>
2023-09-10 00:50:41 +0600

    route part and validation part commited

--- src/app/modules/offered_course_class_schedule/offered_course_class_schedule_route.ts
+++ src/app/modules/offered_course_class_schedule/offered_course_class_schedule_route.ts
@@ -1,11 +1,28 @@
 import { Router } from 'express';
-import { createOfferedCourseClassSchedule } from './offered_course_class_schedule_controller';
+import validateRequest from '../../middlewares/validateRequest';
+import {
+  createOfferedCourseClassSchedule,
+  deleteSingeOfferedCourseClassSchedule,
+  getAllOfferedCourseClassSchedule,
+  getSingleOfferedCourseClassSchedule,
+  updateOfferedCourseClassSchedule,
+} from './offered_course_class_schedule_controller';
+import { create } from './offered_course_class_schedule_validation';
 
 const router = Router();
 
 router.post(
   '/create-offered-course-schedule',
+  validateRequest(create),
   createOfferedCourseClassSchedule
 );
+
+router.get('/', getAllOfferedCourseClassSchedule);
+
+router.get('/:id', getSingleOfferedCourseClassSchedule);
+
+router.patch('/:id', updateOfferedCourseClassSchedule);
+
+router.delete('/:id', deleteSingeOfferedCourseClassSchedule);
 
 export const OfferedCourseClassSchedule = router;
---
+++ src/app/modules/offered_course_class_schedule/offered_course_class_schedule_validation.ts
@@ -0,0 +1,17 @@
+import { z } from 'zod';
+
+export const create = z.object({
+  body: z.object({
+    startTime: z.string({ required_error: 'Start time is required !' }),
+    endTime: z.string({ required_error: 'End time is required !' }),
+    dayOfWeek: z.string({ required_error: 'Day of week is required !' }),
+    offeredCourseSectionId: z.string({
+      required_error: 'Offered course section id is required !',
+    }),
+    semseterRegistrationId: z.string({
+      required_error: ' semester registration id is required !',
+    }),
+    roomId: z.string({ required_error: ' Room id is required !' }),
+    facultyId: z.string({ required_error: 'Faculty id is required' }),
+  }),
+});
