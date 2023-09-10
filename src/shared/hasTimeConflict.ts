49a72792be8639eede9e2160b88ad259512ff87c
mr-sazzad <main.sazza@gmail.com>
2023-09-10 09:36:50 +0600

    StudentSemesterRegistration model commited

--- src/shared/hasTimeConflict.ts
+++ src/shared/hasTimeConflict.ts
@@ -16,11 +16,12 @@ export const hasTimeConflict = (
   newSlot: NewSlot,
   existingSlots: ExistingSlots
 ) => {
+  const newStart = new Date(`1970-01-01T${newSlot.startTime}:00`);
+  const newEnd = new Date(`1970-01-01T${newSlot.endTime}:00`);
+
   for (const slot of existingSlots) {
     const existingStart = new Date(`1970-01-01T${slot.startTime}:00`);
     const existingEnd = new Date(`1970-01-01T${slot.endTime}:00`);
-    const newStart = new Date(`1970-01-01T${newSlot.startTime}:00`);
-    const newEnd = new Date(`1970-01-01T${newSlot.endTime}:00`);
 
     if (existingEnd > newStart && newEnd > existingStart) {
       return true;
